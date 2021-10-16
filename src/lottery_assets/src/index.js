bd89c4caf5722217123bc7846d26f751b98f7e5f
beyond 009 <1249413181@qq.com>
2021-10-16 21:06:48 +0800

    -

--- src/lottery_assets/src/index.js
+++ src/lottery_assets/src/index.js
@@ -7,3 +7,22 @@ document.getElementById("clickMeBtn").addEventListener("click", async () => {
 
   document.getElementById("greeting").innerText = greeting;
 });
+
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+
+  const step = (timestamp) => {
+    //  console.log("timestamp",timestamp)
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    //  console.log("progress",progress)
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+const obj = document.getElementById("value");
+animateValue(obj, 100, 0, 5000);
